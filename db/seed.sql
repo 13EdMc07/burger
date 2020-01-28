84bc0506186b166200da93f73421f0bccb82d26d
E Mc <ekm153a@gmail.com>
2020-01-28 01:53:02 -0600

    burger e

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('chesseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('X Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegi Burger', true);
+-
